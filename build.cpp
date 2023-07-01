80877d8c78cbd8b192a13ff4b7bb214700996289
Eduardo Costa <m4c0@users.noreply.github.com>
2023-07-01 13:49:30 +0100

    skel of per-platform code

--- build.cpp
+++ build.cpp
@@ -3,7 +3,7 @@
 int main(int argc, char **argv) {
   using namespace ecow;
 
-  auto all = unit::create<seq>("all");
+  auto all = unit::create<app>("poc");
   all->add_ref(sitime());
   all->add_unit<>("poc");
   return run_main(all, argc, argv);
--- build.hpp
+++ build.hpp
@@ -4,7 +4,12 @@
 auto sitime() {
   using namespace ecow;
 
-  auto m = unit::create<mod>("sitime");
+  auto m = unit::create<per_feat<mod>>("sitime");
   m->add_wsdep("hai", hai());
+
+  m->for_feature(android_ndk).add_impl("android");
+  m->for_feature(objective_c).add_impl("apple");
+  m->for_feature(windows_api).add_impl("windows");
+  
   return m;
 }
