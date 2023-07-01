9847a9fbf3e58dc528b3348b98be02b9de74a8c7
Eduardo Costa <m4c0@users.noreply.github.com>
2023-07-01 14:31:54 +0100

    windows's implementation

--- windows.cpp
+++ windows.cpp
@@ -1,13 +1,36 @@
+module;
+#include <sysinfoapi.h>
+
 module sitime;
 
 using namespace sitime;
 
-void tp_deleter::operator()(timepoint *) {}
+namespace sitime {
+struct timepoint {
+  ULONGLONG val;
+};
+} // namespace sitime
+
+void tp_deleter::operator()(timepoint *p) { delete p; }
+
+[[nodiscard]] ULARGE_INTEGER current_timestamp() noexcept {
+  FILETIME ft{};
+  GetSystemTimeAsFileTime(&ft);
+
+  // Following rekarks here:
+  // https://learn.microsoft.com/en-us/windows/win32/api/minwinbase/ns-minwinbase-filetime
+  ULARGE_INTEGER uli{};
+  uli.u.LowPart = ft.LowPart;
+  uli.u.HighPart = ft.HighPart;
+  return uli.QuadPart;
+}
 
 stopwatch::ptr stopwatch::current_timestamp() noexcept {
-  return {};
+  return stopwatch::ptr{new timepoint{::current_timestamp()}};
 }
 
 int stopwatch::millis() const noexcept {
-  return 0;
+  auto s = (*m_start)->val;
+  auto e = ::current_timestamp();
+  return static_cast<int>((e - s) / 10);
 }
