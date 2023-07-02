414d81a2031beeed9c050dcd7a21d9a1b975fa33
Eduardo Costa <m4c0@users.noreply.github.com>
2023-07-02 16:37:15 +0100

    poc: improving to help with per-platform issues

--- build.cpp
+++ build.cpp
@@ -1,9 +1,11 @@
+#include "../silog/build.hpp"
 #include "build.hpp"
 
 int main(int argc, char **argv) {
   using namespace ecow;
 
   auto all = unit::create<app>("poc");
+  all->add_wsdep("silog", silog());
   all->add_ref(sitime());
   all->add_unit<>("poc");
   return run_main(all, argc, argv);
--- poc.cpp
+++ poc.cpp
@@ -1,6 +1,11 @@
+import silog;
 import sitime;
+
+extern "C" unsigned sleep(unsigned);
 
 int main() {
   sitime::stopwatch w{};
-  return w.millis();
+  silog::log(silog::info, "Before sleeping: %d", w.millis());
+  sleep(1);
+  silog::log(silog::info, "After sleeping: %d", w.millis());
 }
