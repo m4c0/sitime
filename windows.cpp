a619b235e529c9600e8bdecbdd3fca0dcae34734
Eduardo Costa <m4c0@users.noreply.github.com>
2023-07-02 17:42:50 +0100

    windows: fix (weird) time scale

--- windows.cpp
+++ windows.cpp
@@ -30,9 +30,15 @@ stopwatch::ptr stopwatch::current_timestamp() noexcept {
 }
 
 int stopwatch::millis() const noexcept {
+  // GetSystemTimeAsFileTime returns time in 100-nanosecond intervals since
+  // 1601. Sounds like some senior engineer went very creative. Maybe I can find
+  // a reason for the 100ns precision, but 1601? Why? Who needs that in
+  // FILETIME? Doesn't matter here, but still... Why???
+  constexpr const auto ft_scale = 10 * 1000;
+
   auto s = (*m_start)->val;
   auto e = ::current_timestamp();
-  return static_cast<int>((e - s) / 10);
+  return static_cast<int>((e - s) / ft_scale);
 }
 
 void sitime::sleep(unsigned secs) noexcept { SleepEx(secs * 1000, TRUE); }
