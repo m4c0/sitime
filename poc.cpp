00ebe76c5be4e1d47fa027d1a2dc6275fe7729b6
Eduardo Costa <m4c0@users.noreply.github.com>
2023-07-02 17:26:56 +0100

    poc: increasing the test acceptable range

--- poc.cpp
+++ poc.cpp
@@ -6,6 +6,8 @@ int main() {
   silog::log(silog::info, "Before sleeping: %d", w.millis());
   sitime::sleep(1);
   silog::log(silog::info, "After sleeping: %d", w.millis());
-  if (w.millis() < 990 || w.millis() > 1100)
+  // Giving out some space, because this actually when up to 1190 in a
+  // particular test execution.
+  if (w.millis() < 990 || w.millis() > 1300)
     throw "Time scale is not correct";
 }
