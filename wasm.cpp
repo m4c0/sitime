30f27bfdbc318a01da8407991fbc441e544b47fb
Eduardo Costa <m4c0@users.noreply.github.com>
2024-08-04 22:12:40 +0100

    wasm: finally implementing it

--- wasm.cpp
+++ wasm.cpp
@@ -1,9 +1,31 @@
 module sitime;
+import traits;
 
 using namespace sitime;
+using namespace traits::ints;
 
-void tp_deleter::operator()(timepoint *) {}
+namespace sitime {
+  struct timepoint { uint64_t val; };
+}
 
-stopwatch::ptr stopwatch::current_timestamp() { return {}; }
+void tp_deleter::operator()(timepoint *p) { delete p; }
 
-int stopwatch::millis() const { return 0; }
+extern "C" void clock_time_get(int, uint64_t, uint64_t * out) __attribute__((import_module("wasi_snapshot_preview1"), import_name("clock_time_get"))) ;
+
+static uint64_t current_timestamp() {
+  uint64_t x{};
+  clock_time_get(0, 0, &x);
+  return x;
+}
+
+stopwatch::ptr stopwatch::current_timestamp() {
+  return stopwatch::ptr{new timepoint{::current_timestamp()}};
+}
+
+int stopwatch::millis() const {
+  constexpr const auto ft_scale = 1000 * 1000;
+
+  auto s = (*m_start)->val;
+  auto e = ::current_timestamp();
+  return static_cast<int>((e - s) / ft_scale);
+}
