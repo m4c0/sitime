f14861696cffd03e24d713cf7c8ecef9e03023e8
Eduardo Costa <m4c0@users.noreply.github.com>
2024-08-05 14:12:23 +0100

    wasm: using some vaselin

--- poc-wasm.cpp
+++ poc-wasm.cpp
@@ -4,16 +4,12 @@
 
 import silog;
 import sitime;
-
-extern "C"
-    __attribute__((import_module("leco"), import_name("set_timeout"))) void
-    set_timeout(void (*)(), int);
+import vaselin;
 
 static sitime::stopwatch w{};
 
-struct init { init(); } i;
-init::init() {
+int main() {
   silog::log(silog::info, "Before sleeping: %d", w.millis());
-  set_timeout([] { silog::log(silog::info, "After sleeping: %d", w.millis()); },
+  vaselin::set_timeout([] { silog::log(silog::info, "After sleeping: %d", w.millis()); },
               1000);
 }
