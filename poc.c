dbdda7e9801b0899c0fd72e4e41f211395f71162
Eduardo Costa <m4c0@users.noreply.github.com>
2026-05-03 12:20:36 +0100

    extending C to "unify" posix/windows into timeval

--- poc.c
+++ poc.c
@@ -1,6 +1,8 @@
 #ifdef _WIN32
 #define WIN32_MEAN_AND_LEAN
 #include <windows.h>
+#else
+#include <sys/time.h>
 #endif
 
 #include <assert.h>
@@ -8,12 +10,14 @@
 #include <stdio.h>
 
 int main() {
-  uint64_t ts = 0;
+  struct timeval tv = {0};
 
 #ifdef _WIN32
   SYSTEMTIME st;
   GetSystemTime(&st);
 
+  // Contains a 64-bit value representing the number of 100-nanosecond
+  // intervals since January 1, 1601 (UTC).
   FILETIME ft;
   assert(SystemTimeToFileTime(&st, &ft));
 
@@ -21,8 +25,11 @@ int main() {
   i.u.LowPart  = ft.dwLowDateTime;
   i.u.HighPart = ft.dwHighDateTime;
 
-  ts = i.QuadPart;
+  tv.tv_sec = i.QuadPart / (100 * 1000 * 1000);
+  tv.tv_usec = (i.QuadPart / 100) % (1000 * 1000);
+#else
+  gettimeofday(&tv, NULL);
 #endif
 
-  printf("Timestamp = %lld\n", ts);
+  printf("Timestamp = %ld %ld\n", tv.tv_sec, tv.tv_usec);
 }
